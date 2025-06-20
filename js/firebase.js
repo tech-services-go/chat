18f992ecdc159c4be1d33e8cacdb5082e471ea6d
M Abdullah <tech.services.go@gmail.com>
2025-06-20 09:49:12 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,13 +6,15 @@ const firebaseConfig = {
   storageBucket: "realtime-chat-go.appspot.com",
   messagingSenderId: "1045897341591",
   appId: "1:1045897341591:web:d5c48955d86eefe01a97d2",
-  measurementId: "G-KVV2M7Y70W"
+  measurementId: "G-KVV2M7Y70W",
+  databaseURL: "https://realtime-chat-go.firebaseio.com"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const rtdb = firebase.database();
 const provider = new firebase.auth.GoogleAuthProvider();
 
 // Enable analytics (optional)
